fd21ffd6146fbef60f14bfe1d49853fec99f1280
Senitha-Shehan <senitha200109@gmail.com>
2025-02-25 23:36:21 +0530

    Changes in Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
-import Navbar from './component/Navbar'
-import { Route, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import Footer from './component/Footer'
-import UserDetails from './component/UserDetails/UserDetails'
-import AddUser from './component/AddUser/AddUser'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./component/Navbar";
+import Footer from "./component/Footer";
+import HomePage from "./pages/HomePage";
+import UserDetails from "./component/UserDetails/UserDetails";
+import AddUser from "./component/AddUser/AddUser";
 
 const App = () => {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
+      {/* Navbar at the top */}
       <Navbar />
 
-      <Routes>
-        <Route path="/Home" element={<HomePage />} />
-        <Route path="/UserDetails" element={<UserDetails />} />
-        <Route path="/AddUser" element={<AddUser />} />
-      </Routes>
+      {/* Main content (grow to take available space) */}
+      <main className="flex-grow container mx-auto p-6">
+        <Routes>
+          <Route path="/Home" element={<HomePage />} />
+          <Route path="/UserDetails" element={<UserDetails />} />
+          <Route path="/AddUser" element={<AddUser />} />
+        </Routes>
+      </main>
 
+      {/* Footer at the bottom */}
       <Footer />
-      
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
