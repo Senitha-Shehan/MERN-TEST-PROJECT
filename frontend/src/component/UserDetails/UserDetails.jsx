441c193c1e806e5cd8c83b9a107c27de443a0622
Senitha-Shehan <senitha200109@gmail.com>
2025-02-25 18:39:27 +0530

    Update UserDetails.jsx

--- frontend/src/component/UserDetails/UserDetails.jsx
+++ frontend/src/component/UserDetails/UserDetails.jsx
@@ -57,7 +57,6 @@ const UserDetails = () => {
               <th className="border border-gray-300 px-4 py-2">#</th>
               <th className="border border-gray-300 px-4 py-2">Name</th>
               <th className="border border-gray-300 px-4 py-2">Age</th>
-              <th className="border border-gray-300 px-4 py-2">Vmail</th>
               <th className="border border-gray-300 px-4 py-2">Email</th>
               <th className="border border-gray-300 px-4 py-2">Actions</th>
             </tr>
